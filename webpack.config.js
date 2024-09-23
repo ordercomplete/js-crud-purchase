2094cc248108545fd2f16a672fbe82afc6cef359
ordercomplete <ordercomplete777@gmail.com>
2024-09-23 23:14:44 +0300

    create purchase-product

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   entry: "./src/route/index.js",
   output: {
-    path: path.resolve(__dirname, "public/css"),
+    path: path.resolve(__dirname, "public/"),
     publicPath: "/public/dist",
     filename: "js/app.js",
   },
